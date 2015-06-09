1a4666b43e8a7f8f720f4371292d25a9328c808e
Flavien Raynaud <flavien.raynaud@gmail.com>
2015-06-09 22:17:24 +0200

    initial commit

---
+++ lib/capistrano/six_pm.rb
@@ -0,0 +1,7 @@
+require "capistrano/six_pm/version"
+
+module Capistrano
+  module SixPm
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/six_pm/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module SixPm
+    VERSION = "0.1.0"
+  end
+end
